0e090757ab7fd080ba5664a7e87f18a7281c3a40
Seyma Özkan <94443492+seymzkn@users.noreply.github.com>
2022-03-10 19:36:02 +0100

    api

---
+++ src/test/java/get_http_request/GetRequest22.java
@@ -0,0 +1,97 @@
+package get_http_request;
+
+import base_url.HerOkuAppBaseUrl;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+import test_data.HerOkuAppTestData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public class GetRequest22 extends HerOkuAppBaseUrl {
+
+    /*
+    https://restful-booker.herokuapp.com/booking/47
+       {
+           "firstname": "Ali",
+           "lastname": "Can",
+           "totalprice": 500,
+           "depositpaid": true,
+           "bookingdates": {
+               "checkin": "2022-02-01",
+               "checkout": "2022-02-11"
+          }
+       }
+    1) JsonPhat
+    2) De-Serialization
+    */
+
+    @Test
+    public void test22(){
+        //1-URL olustur
+        spec05.pathParams("first","booking","second",17);
+
+        //2-ExpectedData olustir
+        HerOkuAppTestData expectedObje=new HerOkuAppTestData();
+        HashMap<String,Object> expectedTestDataMap=expectedObje.setUpTestData();
+        System.out.println("TEST DATA ICINDEKI EXPECTED DATA" + expectedTestDataMap);
+
+        // {firstname=Ali,
+        // bookingdates={
+        //               checkin=2022-02-01,
+        //               checkout=2022-02-11},
+        // totalprice=500,
+        // depositpaid=true,
+        // lastname=Can}
+
+        //3-request VE responce dogrulama
+
+        Response response=given().spec(spec05).when().get("/{first}/{second}");
+        response.prettyPrint();
+
+        //4- DOGRULAMA
+
+        HashMap<String,Object>actualData=response.as(HashMap.class);
+        //Json formatindaki datayi HashMap e donusturur
+        System.out.println("ACTUAL DATA" + actualData);
+        //{firstname=Ali,
+        // bookingdates={
+        //              checkin=2022-02-01,
+        //              checkout=2022-02-11},
+        //              totalprice=500,
+        //              depositpaid=true,
+        //              lastname=Can}
+
+
+        Assert.assertEquals(expectedTestDataMap.get("firstname"),actualData.get("firstname"));
+        Assert.assertEquals(expectedTestDataMap.get("lastname"),actualData.get("lastname"));
+        Assert.assertEquals(expectedTestDataMap.get("totalprice"),actualData.get("totalprice"));
+        Assert.assertEquals(expectedTestDataMap.get("depositpaid"),actualData.get("depositpaid"));
+        Assert.assertEquals(((Map)expectedTestDataMap.get("bookingdates")).get("checkin"),
+                ((Map)actualData.get("bookingdates")).get("checkin"));
+        Assert.assertEquals(((Map)expectedTestDataMap.get("bookingdates")).get("checkout"),
+                ((Map)actualData.get("bookingdates")).get("checkout"));
+
+
+        //2. YOL JSON PATH
+
+        JsonPath json=response.jsonPath();
+        Assert.assertEquals(expectedTestDataMap.get("firstname"),json.getString("firstname"));
+        Assert.assertEquals(expectedTestDataMap.get("lastname"),json.getString("lastname"));
+        Assert.assertEquals(expectedTestDataMap.get("totalprice"),json.getInt("totalprice"));
+        Assert.assertEquals(expectedTestDataMap.get("depositpaid"),json.getBoolean("depositpaid"));
+        Assert.assertEquals(((Map<?, ?>) expectedTestDataMap.get("bookingdates")).get("checkin"),json.getString("bookingdates.checkin"));
+        Assert.assertEquals(((Map<?, ?>) expectedTestDataMap.get("bookingdates")).get("checkout"),json.getString("bookingdates.checkout"));
+
+
+
+    }
+
+
+
+
+}
