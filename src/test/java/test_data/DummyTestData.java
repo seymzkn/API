4b289ec43f1f44e7da5ed65ef8589791e14dfbbe
Seyma Özkan <94443492+seymzkn@users.noreply.github.com>
2022-03-18 09:24:48 +0100

    pojo/delete

--- src/test/java/test_data/DummyTestData.java
+++ src/test/java/test_data/DummyTestData.java
@@ -96,6 +96,23 @@ public class DummyTestData {
 
     }
 
+    /*
+   http://dummy.restapiexample.com/api/v1/delete/2 bir DELETE request gönderdiğimde
+   Dönen response un status kodunun 200 ve body kısmının aşağıdaki gibi olduğunu test edin
+   {
+   "status": "success",
+   "data": "2",
+   "message": "Successfully! Record has been deleted"
+   }
+  */
+    public JSONObject setUpDeleteExpectedData(){
+        JSONObject expectedData = new JSONObject();
+        expectedData.put("status", "success");
+        expectedData.put("data", "2");
+        expectedData.put("message", "Successfully! Record has been deleted");
+        return expectedData;
+    }
+
 
 
 }
